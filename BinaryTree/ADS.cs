6c662334d7914320132d8bdc6e0d29e2da7f71f6
Christian <christianjjmohan@gmail.com>
2018-04-20 10:29:37 -0400

    things

--- BinaryTree/ADS.cs
+++ BinaryTree/ADS.cs
@@ -8,9 +8,9 @@ namespace ADS
 {
     public enum TraverseOrder
     {
-       InOrder,
-       PreOrder,
-       PostOrder
+        InOrder,
+        PreOrder,
+        PostOrder
     }
 
     public class ADSTree
@@ -28,26 +28,102 @@ namespace ADS
 
         public ADSTree()
         {
-           root = null;
+            root = null;
         }
 
         // Return the node where value is located
         public ADSNode find(int value)
         {
-            
-            
             return null;
+        }
+
+        public void insert(ADSNode parent, int value)
+        {
+            if (value > parent.key)
+            {
+                if (parent.right == null)
+                {
+                    // Create a new node connected to parent.right
+                }
+                else
+                {
+                    insert(parent.right, value);
+
+                    parent.height = Math.Max(parent.left == null ? 0 : parent.left.height,
+                                             parent.right == null ? 0 : parent.right.height) + 1;
+
+                }
+            }
+            else if (value < parent.key)
+            {
+                if (parent.left == null)
+                {
+                    // create a new node connected to parent.left
+                }
+                else
+                {
+                    insert(parent.left, value);
+
+                    parent.height = Math.Max(parent.left == null ? 0 : parent.left.height,
+                                             parent.right == null ? 0 : parent.right.height) + 1;
+
+                }
+
+            }
         }
 
         // Inserts a node into the tree and maintains it's balance
         public void insert(int value)
         {
 
+
+            ADSNode n = new ADSNode();
+            n.left = null;
+            n.right = null;
+            n.key = value;
+            n.cardinality = 0;
+            n.height = 0;
+
+            ADSNode node = root;
+
+            while (true)
+            {
+
+
+                if (value > node.key)
+                {
+                    if (node.right == null)
+                    {
+                        node.right = n;
+                        break;
+                    }
+                    else
+                        node = node.right;
+                }
+                else if (value == root.key)
+                {
+                    node.cardinality++;
+                }
+                else if (value < root.key)
+                {
+                    root.left = n;
+                }
+            }
+        }
+
+        public void printTree(ADSNode node, TraverseOrder order)
+        {
+            if (node == null) return;
+
+            printTree(node.left, order);
+            if (order == TraverseOrder.InOrder) Console.Write(node.key);
+            printTree(node.right, order);
         }
 
         // Print the tree in a particular order
         public void printTree(TraverseOrder order)
         {
+            printTree(root, order);
         }
     }
 }
